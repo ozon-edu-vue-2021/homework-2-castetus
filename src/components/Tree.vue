9e5bb802a6c71a8a39386d84ff160da64d1ad140
castetus <kboikov@mail.ru>
2021-10-26 10:29:17 +0300

    feat: add keyboard navigation

--- src/components/Tree.vue
+++ src/components/Tree.vue
@@ -1,36 +1,60 @@
 <template>
-  <div class="tree">
-    <menu-icon />
+  <div class="tree" @keyup.prevent="keyboardHandler" tabindex="0">
+    <div class="tree__full-path">Fullpath: {{fullPath}}</div>
     <tree-item
-      :item="treeData"></tree-item>
+      :root="true"
+      :item="treeData">
+    </tree-item>
   </div>
 </template>
 
 <script>
 import TreeItem from './TreeItem.vue';
 import treeData from '../../public/static/node_modules.json';
-import MenuIcon from 'vue-material-design-icons/Menu.vue';
+import { eventBus } from '../main';
 
 export default {
   name: 'Tree',
   components: {
     TreeItem,
-    MenuIcon,
   },
   data() {
     return {
       treeData: [],
+      fullPath: '',
+      controlKeys: [
+        'Enter',
+        'ArrowUp',
+        'ArrowDown',
+        'ArrowLeft',
+        'ArrowRight',
+      ],
     };
   },
   created() {
     this.treeData = treeData;
+    eventBus.$on('setFullPath', (path) => this.fullPath = path);
+  },
+  mounted() {
+    this.$el.focus();
+  },
+  methods: {
+    keyboardHandler(ev) {
+      if (!this.controlKeys.includes(ev.code)) {
+        return;
+      }
+      eventBus.$emit('keyEvent', ev.code);
+    },
   },
 }
 </script>
 
 <style lang="scss">
   .tree{
+    outline: none;
     &__item{
+      margin: 8px 0;
+      width: fit-content;
       &_directory{
         font-weight: bold;
       }
@@ -45,6 +69,16 @@ export default {
     }
     &__toggler{
       cursor: pointer;
+    }
+    &__full-path{
+      margin-bottom: 20px;
+    }
+    &__text{
+      padding: 5px;
+      &_selected{
+        border: 1px dotted grey;
+        background: rgba($color: grey, $alpha: 0.15);
+      }
     }
   }
 </style>
